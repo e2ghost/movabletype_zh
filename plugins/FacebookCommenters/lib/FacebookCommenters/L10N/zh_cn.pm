69e1686e21d1fda37c74a610a07f069cfdfc0bed
e2ghost <e2ghost@gmail.com>
2013-12-11 17:29:54 +0800

    Facebook Commenters插件語言修改

--- plugins/FacebookCommenters/lib/FacebookCommenters/L10N/zh_cn.pm
+++ plugins/FacebookCommenters/lib/FacebookCommenters/L10N/zh_cn.pm
@@ -13,25 +13,25 @@ use vars qw( %Lexicon );
 %Lexicon = (
 
 ## plugins/FacebookCommenters/config.yaml
-	'Provides commenter registration through Facebook Connect.' => 'Facebookコネクトを利用したコメント投稿者の登録機能を提供します。',
+	'Provides commenter registration through Facebook Connect.' => '评论人可以通过 Facebook 连接注册/登录。',
 	'Facebook' => 'Facebook',
 
 ## plugins/FacebookCommenters/lib/FacebookCommenters/Auth.pm
-	'Set up Facebook Commenters plugin' => 'Facebook Commentersプラグイン設定',
-	'Authentication failure: [_1], reason:[_2]' => '認証に失敗しました: [_1], 理由:[_2]',
-	'Failed to created commenter.' => 'コメンターの作成に失敗しました。',
-	'Failed to create a session.' => 'コメンターセッションの作成に失敗しました。',
-	'Facebook Commenters needs either Crypt::SSLeay or IO::Socket::SSL installed to communicate with Facebook.' => 'Facebook Commenters を利用するには、Crypt::SSLeay または IO::Socket::SSLのいずれかがインストールされている必要があります。',
-	'Please enter your Facebook App key and secret.' => 'FacebookアプリケーションキーとFacebookアプリケーションシークレットを入力してください。',
-	'Could not verify this app with Facebook: [_1]' => 'Facebookでこのアプリケーションを確認できません: [_1]',
+	'Set up Facebook Commenters plugin' => '设置 Facebook 评论插件',
+	'Authentication failure: [_1], reason:[_2]' => '认证失败: [_1], 原因:[_2]',
+	'Failed to created commenter.' => '创建评论者失败。',
+	'Failed to create a session.' => '创建会话失败。',
+	'Facebook Commenters needs either Crypt::SSLeay or IO::Socket::SSL installed to communicate with Facebook.' => 'Facebook Commenters需要借助Crypt::SSLeay或IO::Socket::SSL来与Facebook通讯。',
+	'Please enter your Facebook App key and secret.' => '请输入您的Facebook App Key和secret。',
+	'Could not verify this app with Facebook: [_1]' => 'Facebook无法验证此App: [_1]',
 
 ## plugins/FacebookCommenters/tmpl/blog_config_template.tmpl
-	'Facebook Application Key' => 'Facebookアプリケーションキー',
-	'The key for the Facebook application associated with your blog.' => 'ブログ関連付用Facebookアプリケーションキー',
-	'Edit Facebook App' => 'Facebookアプリ編集',
-	'Create Facebook App' => 'Facebookアプリ作成',
-	'Facebook Application Secret' => 'Facebookアプリケーションシークレット',
-	'The secret for the Facebook application associated with your blog.' => 'ブログ関連付用Facebookアプリケーションシークレット',
+	'Facebook Application Key' => 'Facebook App Key',
+	'The key for the Facebook application associated with your blog.' => '用来关联你的 Blog 和 Facebook 应用的 Key',
+	'Edit Facebook App' => '编辑Facebook应用',
+	'Create Facebook App' => '创建Facebook应用',
+	'Facebook Application Secret' => 'Facebook App Secret',
+	'The secret for the Facebook application associated with your blog.' => '用来关联你的 Blog 和 Facebook 应用的 Secret',
 
 );
 
--- plugins/FacebookCommenters/lib/FacebookCommenters/L10N/zh_tw.pm
+++ plugins/FacebookCommenters/lib/FacebookCommenters/L10N/zh_tw.pm
@@ -13,25 +13,25 @@ use vars qw( %Lexicon );
 %Lexicon = (
 
 ## plugins/FacebookCommenters/config.yaml
-	'Provides commenter registration through Facebook Connect.' => 'Facebookコネクトを利用したコメント投稿者の登録機能を提供します。',
+	'Provides commenter registration through Facebook Connect.' => '評論人可以通過 Facebook 連接註冊/登入。',
 	'Facebook' => 'Facebook',
 
 ## plugins/FacebookCommenters/lib/FacebookCommenters/Auth.pm
-	'Set up Facebook Commenters plugin' => 'Facebook Commentersプラグイン設定',
-	'Authentication failure: [_1], reason:[_2]' => '認証に失敗しました: [_1], 理由:[_2]',
-	'Failed to created commenter.' => 'コメンターの作成に失敗しました。',
-	'Failed to create a session.' => 'コメンターセッションの作成に失敗しました。',
-	'Facebook Commenters needs either Crypt::SSLeay or IO::Socket::SSL installed to communicate with Facebook.' => 'Facebook Commenters を利用するには、Crypt::SSLeay または IO::Socket::SSLのいずれかがインストールされている必要があります。',
-	'Please enter your Facebook App key and secret.' => 'FacebookアプリケーションキーとFacebookアプリケーションシークレットを入力してください。',
-	'Could not verify this app with Facebook: [_1]' => 'Facebookでこのアプリケーションを確認できません: [_1]',
+	'Set up Facebook Commenters plugin' => '設定 Facebook 評論插件',
+	'Authentication failure: [_1], reason:[_2]' => '驗證失敗: [_1], 原因:[_2]',
+	'Failed to created commenter.' => '新增評論者失敗。',
+	'Failed to create a session.' => '新增Session失敗。',
+	'Facebook Commenters needs either Crypt::SSLeay or IO::Socket::SSL installed to communicate with Facebook.' => 'Facebook Commenters需要透過Crypt::SSLeay或IO::Socket::SSL來與Facebook通訊。',
+	'Please enter your Facebook App key and secret.' => '請輸入您的Facebook App Key和secret。',
+	'Could not verify this app with Facebook: [_1]' => 'Facebook無法驗證此App: [_1]',
 
 ## plugins/FacebookCommenters/tmpl/blog_config_template.tmpl
-	'Facebook Application Key' => 'Facebookアプリケーションキー',
-	'The key for the Facebook application associated with your blog.' => 'ブログ関連付用Facebookアプリケーションキー',
-	'Edit Facebook App' => 'Facebookアプリ編集',
-	'Create Facebook App' => 'Facebookアプリ作成',
-	'Facebook Application Secret' => 'Facebookアプリケーションシークレット',
-	'The secret for the Facebook application associated with your blog.' => 'ブログ関連付用Facebookアプリケーションシークレット',
+	'Facebook Application Key' => 'Facebook App Key',
+	'The key for the Facebook application associated with your blog.' => '用來關聯您的 Blog 和 Facebook 應用的 Key',
+	'Edit Facebook App' => '修改Facebook應用',
+	'Create Facebook App' => '新增Facebook應用',
+	'Facebook Application Secret' => 'Facebook App Secret',
+	'The secret for the Facebook application associated with your blog.' => '用來關聯您的 Blog 和 Facebook 應用的 Secret',
 
 );
 
